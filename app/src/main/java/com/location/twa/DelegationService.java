e1fbe09e7982a60b9eee6a42da021e98b1cf1d65
clonez9494 <clonez9494@gmail.com>
2023-01-26 00:08:38 +0700

    coba-twa-chrome-helper

---
+++ app/src/main/java/com/location/twa/DelegationService.java
@@ -0,0 +1,13 @@
+package com.location.twa;
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+public class DelegationService extends com.google.androidbrowserhelper.trusted.DelegationService {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+    }
+}
+
